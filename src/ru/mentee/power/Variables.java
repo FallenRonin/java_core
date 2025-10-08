f9f374fc82d9013777b755a13c653dc08b079554
FallenRonin <FallenRonin@yandex.ru>
2025-10-08 18:00:06 +0300

    MP-18: moved to Gradle build system added Variables.java class updated .gitignore

---
+++ src/ru/mentee/power/Variables.java
@@ -0,0 +1,9 @@
+package ru.mentee.power;
+
+public class Variables {
+    public static void main(String[] args) {
+        int myAge = 25;
+        String myName = "Alexandr";
+        System.out.printf("Меня зовут %s, мне %d лет", myName, myAge);
+    }
+}
