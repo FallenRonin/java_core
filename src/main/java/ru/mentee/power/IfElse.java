ea3efff4d9c4c37b3e944bd1c7950cee478b9eda
FallenRonin <89581468+FallenRonin@users.noreply.github.com>
2025-11-01 16:12:58 +0300

    MP-22: added IfElse.java (#8)

---
+++ src/main/java/ru/mentee/power/IfElse.java
@@ -0,0 +1,26 @@
+package ru.mentee.power;
+
+import java.util.Scanner;
+
+public class IfElse {
+
+    public static void main(String[] args) {
+        int age;
+        boolean hasLicense;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age:");
+        age = scanner.nextInt();
+        System.out.println("Do you have driver's license? yes/no");
+        if (scanner.nextLine().equalsIgnoreCase("yes")) {
+            hasLicense = true;
+        } else {
+            hasLicense = false;
+        }
+        if (age >= 18 && hasLicense){
+            System.out.println("You can rent a car!");
+        }else{
+            System.out.println("Sorry, you can't rent a car.");
+        }
+    }
+
+}
